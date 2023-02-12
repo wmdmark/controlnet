19ce9aae0c9d229dd40f03aabd0948354e6c85be
Jasper <37783831+jagilley@users.noreply.github.com>
2023-02-12 00:05:11 +0000

    canny edge detection mainly

--- gradio_canny2image.py
+++ gradio_canny2image.py
@@ -14,9 +14,9 @@ from cldm.model import create_model, load_state_dict
 from ldm.models.diffusion.ddim import DDIMSampler
 
 
-model = create_model('./models/cldm_v15.yaml').cuda()
-model.load_state_dict(load_state_dict('./models/control_sd15_canny.pth', location='cuda'))
-ddim_sampler = DDIMSampler(model)
+# model = create_model('./models/cldm_v15.yaml').cuda()
+# model.load_state_dict(load_state_dict('./models/control_sd15_canny.pth', location='cuda'))
+# ddim_sampler = DDIMSampler(model)
 
 
 def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, scale, seed, eta, low_threshold, high_threshold):
@@ -48,31 +48,31 @@ def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resoluti
     return [255 - detected_map] + results
 
 
-block = gr.Blocks().queue()
-with block:
-    with gr.Row():
-        gr.Markdown("## Control Stable Diffusion with Canny Edge Maps")
-    with gr.Row():
-        with gr.Column():
-            input_image = gr.Image(source='upload', type="numpy")
-            prompt = gr.Textbox(label="Prompt")
-            run_button = gr.Button(label="Run")
-            with gr.Accordion("Advanced options", open=False):
-                num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-                image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=256)
-                low_threshold = gr.Slider(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1)
-                high_threshold = gr.Slider(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1)
-                ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
-                seed = gr.Slider(label="Seed", minimum=0, maximum=2147483647, step=1, randomize=True)
-                eta = gr.Number(label="eta (DDIM)", value=0.0)
-                a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed')
-                n_prompt = gr.Textbox(label="Negative Prompt",
-                                      value='longbody, lowres, bad anatomy, bad hands, missing fingers, pubic hair,extra digit, fewer digits, cropped, worst quality, low quality')
-        with gr.Column():
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, scale, seed, eta, low_threshold, high_threshold]
-    run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
+# block = gr.Blocks().queue()
+# with block:
+#     with gr.Row():
+#         gr.Markdown("## Control Stable Diffusion with Canny Edge Maps")
+#     with gr.Row():
+#         with gr.Column():
+#             input_image = gr.Image(source='upload', type="numpy")
+#             prompt = gr.Textbox(label="Prompt")
+#             run_button = gr.Button(label="Run")
+#             with gr.Accordion("Advanced options", open=False):
+#                 num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
+#                 image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=256)
+#                 low_threshold = gr.Slider(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1)
+#                 high_threshold = gr.Slider(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1)
+#                 ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
+#                 scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
+#                 seed = gr.Slider(label="Seed", minimum=0, maximum=2147483647, step=1, randomize=True)
+#                 eta = gr.Number(label="eta (DDIM)", value=0.0)
+#                 a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed')
+#                 n_prompt = gr.Textbox(label="Negative Prompt",
+#                                       value='longbody, lowres, bad anatomy, bad hands, missing fingers, pubic hair,extra digit, fewer digits, cropped, worst quality, low quality')
+#         with gr.Column():
+#             result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
+#     ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, scale, seed, eta, low_threshold, high_threshold]
+#     run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
 
 
-block.launch(server_name='0.0.0.0')
+# block.launch(server_name='0.0.0.0')
--- predict.py
+++ predict.py
@@ -1,42 +1,82 @@
 # Prediction interface for Cog ⚙️
-# https://github.com/replicate/cog/blob/main/docs/python.md
+# https://github.com/replicate/cog/resolve/main/docs/python.md
 
 from cog import BasePredictor, Input, Path
+import os
+from subprocess import call
+from cldm.model import create_model, load_state_dict
+from ldm.models.diffusion.ddim import DDIMSampler
+
+from gradio_canny2image import process
 
 model_dl_urls = {
-    "control_sd15_canny.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_canny.pth",
-    "control_sd15_depth.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_depth.pth",
-    "control_sd15_hed.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_hed.pth",
-    "control_sd15_normal.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_normal.pth",
-    "control_sd15_mlsd.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_mlsd.pth",
-    "control_sd15_openpose.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_openpose.pth",
-    "control_sd15_scribble.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_scribble.pth",
-    "control_sd15_seg.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/models/control_sd15_seg.pth",
+    "canny": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_canny.pth",
+    "depth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_depth.pth",
+    "hed": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_hed.pth",
+    "normal": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_normal.pth",
+    "mlsd": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_mlsd.pth",
+    "openpose": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_openpose.pth",
+    "scribble": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_scribble.pth",
+    "seg": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_seg.pth",
 }
 
 annotator_dl_urls = {
-    "body_pose_model.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/body_pose_model.pth",
-    "dpt_hybrid-midas-501f0c75.pt": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/dpt_hybrid-midas-501f0c75.pt",
-    "hand_pose_model.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/hand_pose_model.pth",
-    "mlsd_large_512_fp32.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/mlsd_large_512_fp32.pth",
-    "mlsd_tiny_512_fp32.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/mlsd_tiny_512_fp32.pth",
-    "network-bsds500.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/network-bsds500.pth",
-    "upernet_global_small.pth": "https://huggingface.co/lllyasviel/ControlNet/blob/main/annotator/ckpts/upernet_global_small.pth",
+    "body_pose_model.pth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/body_pose_model.pth",
+    "dpt_hybrid-midas-501f0c75.pt": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/dpt_hybrid-midas-501f0c75.pt",
+    "hand_pose_model.pth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/hand_pose_model.pth",
+    "mlsd_large_512_fp32.pth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/mlsd_large_512_fp32.pth",
+    "mlsd_tiny_512_fp32.pth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/mlsd_tiny_512_fp32.pth",
+    "network-bsds500.pth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/network-bsds500.pth",
+    "upernet_global_small.pth": "https://huggingface.co/lllyasviel/ControlNet/resolve/main/annotator/ckpts/upernet_global_small.pth",
 }
+
+def download_model(model_name):
+    """
+    Download model from huggingface with wget and save to models directory
+    """
+    if not os.path.exists("models"):
+        os.mkdir("models")
+    if not os.path.exists(f"models/{model_name}"):
+        print(f"Downloading {model_name}...")
+        call(["wget", "-O", f"models/{model_name}", model_dl_urls[model_name]])
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        # self.model = torch.load("./weights.pth")
+        self.model = create_model('./models/cldm_v15.yaml').cuda()
+        self.model.load_state_dict(load_state_dict('./models/control_sd15_canny.pth', location='cuda'))
+        self.ddim_sampler = DDIMSampler(model)
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
-        ),
+        input_image: Path = Input(description="Grayscale input image"),
+        prompt: str = Input(description="Prompt for the model"),
+        # model: str = Input(
+        #     description="Type of model to use",
+        #     choices=["canny", "depth", "hed", "normal", "mlsd", "openpose", "scribble", "seg"],
+        #     default="canny"
+        # ),
+        num_samples: int = Input(description="Number of samples", default=1),
+        image_resolution: int = Input(description="Image resolution", default=512),
+        low_threshold: int = Input(description="Canny low threshold", default=100),
+        high_threshold: int = Input(description="Canny high threshold", default=200),
+        ddim_steps: int = Input(description="Steps", default=20),
+        scale: float = Input(description="Guidance Scale", default=9.0),
+        seed: int = Input(description="Seed", default=0),
+        eta: float = Input(description="eta (DDIM)", default=0.0),
+        a_prompt: str = Input(description="Added Prompt", default="best quality, extremely detailed"),
+        n_prompt: str = Input(description="Negative Prompt", default="longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality"),
     ) -> Path:
         """Run a single prediction on the model"""
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
+        
+        # check that the model is downloaded
+        # if not os.path.exists(f"models/{model}"):
+        #     download_model(model)
+
+        inputs = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, scale, seed, eta, low_threshold, high_threshold]
+        outputs = process(inputs)
+        # outputs from numpy to PIL
+        outputs = Image.fromarray(outputs)
+        # save the output image
+        outputs.save("tmp/output.png")
+        return Path("tmp/output.png")
